cfca8596ba6e93118b4742c25f1ade44cd3dac6b
master <csy21977@gmail.com>
2025-01-14 16:34:35 +0900

    commit

--- src/main/java/repository/UserRepository.java
+++ src/main/java/repository/UserRepository.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User join(User user);
+    User save(User user);
     Optional<User> findByName(String id);
 
 }
