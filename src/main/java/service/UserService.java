1b41fe88a37cd7c05fc6c2ca9aefff487830f6cb
master <csy21977@gmail.com>
2025-01-14 16:15:32 +0900

    commit

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,4 +1,12 @@
 package service;
 
+import org.aspectj.lang.annotation.Around;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import repository.UserRepository;
+
+@Service
 public class UserService {
+    @Autowired
+    private UserRepository userRepository;
 }
