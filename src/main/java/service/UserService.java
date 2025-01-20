a3a9822ab5b6f2ec3c174ca879c9ece148facf42
master <csy21977@gmail.com>
2025-01-20 16:25:56 +0900

    commit

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -3,29 +3,48 @@ package service;
 import domain.User;
 import org.aspectj.lang.annotation.Around;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import repository.UserRepository;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 
 @Service
 public class UserService {
 
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
     @Autowired
-    private UserRepository userRepository;
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder){
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     public void save(User user){
         duplicateCheck(user);
+        user.setPw(encodePassword(user.getPw()));
         userRepository.save(user);
-
     }
 
+    // id 중복체크
     private void duplicateCheck(User user){
         userRepository.findByName(user.getId()).ifPresent( m -> {
-            throw new IllegalStateException("중복된 ID 입니다.");
+            throw new IllegalStateException("이미 사용중인 ID 입니다." + user.getId());
         });
     }
 
+    // pw BCrypt 인코딩
+    public String encodePassword(String rawPassword){
+        return passwordEncoder.encode(rawPassword);
+    }
 
-
+    // 로그인 시 pw 검증
+    public boolean matchesPassword(String rawPassword, String encodePassword){
+        return passwordEncoder.matches(rawPassword, encodePassword);
+    }
 
 }
